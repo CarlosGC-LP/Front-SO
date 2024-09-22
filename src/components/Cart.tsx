952eb783447db6e4c529d65ce48353b4357046f1
davidalexd <davidalexd1234@gmail.com>
2024-09-22 13:13:45 -0500

    add styles carlos

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -21,7 +21,7 @@ export const Cart = () => {
               className={`pointer-events-auto w-screen max-w-md transform transition ease-in-out duration-500 sm:duration-700 ${isSidebarMenuOpen ? 'translate-x-0' : 'translate-x-full'
                 }`}
             >
-              <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+              <div className="flex h-full flex-col overflow-y-scroll bg-purple-700 shadow-xl">
                 <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                   <div className="flex items-start justify-between">
                     <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
@@ -29,8 +29,8 @@ export const Cart = () => {
                       <button onClick={closeSidebarMenu} type="button" className="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
                         <span className="absolute -inset-0.5"></span>
                         <span className="sr-only">Close panel</span>
-                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                       </button>
                     </div>
@@ -51,13 +51,13 @@ export const Cart = () => {
                                 </h3>
                                 <p className="ml-4">$90.00</p>
                               </div>
-                              <p className="mt-1 text-sm text-gray-500">Salmon</p>
+                              <p className="mt-1 text-sm text-yellow-300">Salmon</p>
                             </div>
                             <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">Qty 1</p>
+                              <p className="text-yellow-300">Qty 1</p>
 
                               <div className="flex">
-                                <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                <button type="button" className="font-medium text-yellow-500">Remove</button>
                               </div>
                             </div>
                           </div>
@@ -66,22 +66,21 @@ export const Cart = () => {
                           <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                             <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." className="h-full w-full object-cover object-center" />
                           </div>
-
                           <div className="ml-4 flex flex-1 flex-col">
                             <div>
                               <div className="flex justify-between text-base font-medium text-gray-900">
                                 <h3>
-                                  <a href="#">Medium Stuff Satchel</a>
+                                  <a href="#">Throwback Hip Bag</a>
                                 </h3>
-                                <p className="ml-4">$32.00</p>
+                                <p className="ml-4">$90.00</p>
                               </div>
-                              <p className="mt-1 text-sm text-gray-500">Blue</p>
+                              <p className="mt-1 text-sm text-yellow-300">Salmon</p>
                             </div>
                             <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">Qty 1</p>
+                              <p className="text-yellow-300">Qty 1</p>
 
                               <div className="flex">
-                                <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                <button type="button" className="font-medium text-yellow-500">Remove</button>
                               </div>
                             </div>
                           </div>
@@ -97,9 +96,9 @@ export const Cart = () => {
                     <p>Subtotal</p>
                     <p>$262.00</p>
                   </div>
-                  <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                  <p className="mt-0.5 text-sm text-yellow-500">Precio total incluido</p>
                   <div className="mt-6">
-                    <a href="#" className="flex items-center justify-center rounded-md border border-transparentpx-6 py-3 text-base font-medium text-white shadow-sm  bg-black hover:text-gray-500">Checkout</a>
+                    <a href="#" className="flex items-center justify-center rounded-md border border-transparentpx-6 py-3 text-base font-medium text-black shadow-sm  bg-yellow-300 hover:bg-yellow-500">Pagar</a>
                   </div>
                 </div>
               </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,82 @@
-import { useState } from "react";
-import { Header } from "./Header";
-import { Hero } from "./Hero";
+import { useEffect, useState } from "react";
 import { Cart } from "./Cart";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { useUIStore } from "../store/ui-store";
+import { FaCartShopping, FaUser } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
-
+interface Producto {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    precio: string;
+    cantidad_en_stock: number;
+    urlImgProducto: string;
+}
 export const Home = () => {
 
+    const [productos, setProductos] = useState<Producto[]>([]);
 
+    useEffect(() => {
+        const fetchProductos = async () => {
+            try {
+                const response = await fetch('http://135.224.16.135/api/productos/');
+                if (!response.ok) {
+                    throw new Error('Error al obtener los productos');
+                }
+                const data: Producto[] = await response.json();
+                setProductos(data);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchProductos();
+    }, [])
+
+    const openSidebarMenu = useUIStore(state => state.openSidebarMenu);
     return (
         <div>
-            <Header />
-            <Hero />
-            <Cart  />
+
+            <div className="bg-purple-700 text-[1.6rem] leading-[1.5]">
+                <header className="flex justify-center">
+                    <Link to="/">
+                        <img className="my-8 p-0" width="75" height="75" src="https://img.icons8.com/office/40/shop.png" alt="Logotipo" />
+                    </Link>
+                </header>
+                <nav className="py-4 flex flex-col  sm:flex-row justify-center items-center gap-8 bg-purple-800">
+                    <a className="text-yellow-300 text-[1.7rem] " href="#">Tienda</a>
+                    <a className="text-white text-[1.7rem] 0hover:text-yellow-30 " href="#">Ofertas</a>
+                    <a className="text-white text-[1.7rem] hover:text-yellow-300 " href="#">Mis compras</a>
+                    <button onClick={openSidebarMenu} type="button" className="flex justify-center  gap-2 items-center rounded-full text-white text-[1.7rem] hover:text-yellow-300">
+                        <FaCartShopping size={24} className="text-white" />
+                        Carrito
+                    </button>
+                    <Link to="/login" className="flex justify-center  gap-2 items-center rounded-full text-white text-[1.7rem] hover:text-yellow-300"><FaUser size={24} /> Iniciar sesion</Link>
+                </nav>
+
+                <main className="mx-auto container">
+                    <h1 className="text-center text-yellow-300 text-[2.5rem]  my-8">Nuestros productos</h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-4">
+                        {productos.map((producto, index) => (
+                            <div key={index} className="bg-purple-800 p-4 rounded-xl">
+                                <a href="#">
+                                    <img className="w-full max-h-[400px] rounded-xl" src={producto.urlImgProducto} alt={producto.nombre} />
+                                    <div>
+                                        <p className="text-[2rem] text-white text-center my-4 font-sans">{producto.nombre}</p>
+                                        <p className="text-[2.3rem] text-yellow-300 text-center font-sans">{producto.precio}</p>
+                                    </div>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </main>
+
+                <footer className="py-4 mt-8 bg-purple-800">
+                    <p className="text-center text-[1.8rem] text-white font-sans">Grupo06 - Todos los derechos reservados</p>
+                </footer>
+            </div>
+            <Cart />
         </div>
     )
 }
